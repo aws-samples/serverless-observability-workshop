17b564a2183286bc8c63a40a7d7ba6709b5739a6
Caio Rondon <crondonb@amazon.com>
2023-08-30 12:33:06 -0300

    added python sampleapp SAM project

---
+++ code/powertools/python/SampleApp/src/app.py
@@ -0,0 +1,52 @@
+import os
+import json
+import boto3
+
+from utils.router import Router
+
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+client = boto3.resource('dynamodb')
+table = client.Table(os.environ.get('SAMPLE_TABLE'))
+
+def get_item(**kargs):
+    item_id = int(kargs["event"]["pathParameters"]["proxy"].split("/")[-1])
+    data = table.query(    
+        KeyConditionExpression='Id = :id_val',
+        ExpressionAttributeValues={
+            ':id_val': item_id
+        }
+    )
+    print(data)
+    return {"statusCode": 200, "body": json.dumps(data.get('Items'), cls=DecimalEncoder)}
+
+def list_items(**kargs):
+    data = table.scan()
+    return {"statusCode": 200, "body": json.dumps(data.get('Items'), cls=DecimalEncoder)}
+
+def create_item(**kargs):
+
+    item = json.loads(kargs['event']['body'])
+    
+    data = table.put_item(
+        Item=item
+    )
+    
+    return {"statusCode": 200, "body": json.dumps(item, cls=DecimalEncoder)}
+
+router = Router()
+router.set(path="/api/items/",      method="GET",   handler=list_items)
+router.set(path="/api/items/{id}",  method="GET",   handler=get_item)
+router.set(path="/api/items/",      method="POST",  handler=create_item)
+
+def lambda_handler(event, context):
+    path = event["requestContext"]["http"]["path"]
+    http_method = event["requestContext"]["http"]["method"]
+    method, _ = router.get(path=path, method=http_method)
+    return method(event=event)
