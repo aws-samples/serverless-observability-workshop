56fa04a776f03a38ea064f6178e128e29ce3da90
Girish B <girishpb@amazon.com>
2023-09-24 13:26:29 +0530

    Logging changes

--- code/powertools/java/SampleSolution/src/main/java/com/item/handler/PostItemHandler.java
+++ code/powertools/java/SampleSolution/src/main/java/com/item/handler/PostItemHandler.java
@@ -7,8 +7,9 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.fasterxml.jackson.jr.ob.JSON;
 import com.item.entity.Item;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.core.SdkSystemSetting;
 import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
@@ -16,13 +17,16 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.logging.LoggingUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class PostItemHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final Logger logger = LogManager.getLogger(PostItemHandler.class);
 
-    private final Logger logger = LoggerFactory.getLogger(PostItemHandler.class);
     private final DynamoDbAsyncClient dynamoDbClient;
 
     public PostItemHandler() {
@@ -34,19 +38,30 @@ public class PostItemHandler implements RequestHandler<APIGatewayProxyRequestEve
                 .build();
     }
 
+    @Logging(logEvent = true)
+    @Override 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 
         try {
 
-            logger.info("Received request: {}", JSON.std.asString(input));
+            logger.info("Received request");
             Item item = JSON.std.beanFrom(Item.class, input.getBody());
+            
+            LoggingUtils.appendKey("Item Name: ", item.getName());
+            
+            Map<String, String> additionalInfo = new HashMap<>();
+            additionalInfo.put("Id", String.valueOf(item.getId()));
+            additionalInfo.put("Name", item.getName());
+            LoggingUtils.appendKeys(additionalInfo);
+            logger.info("Request Details");
+
             createItem(item);
 
             return new APIGatewayProxyResponseEvent()
                     .withStatusCode(200)
                     .withBody("Created item: " + JSON.std.asString(item));
         } catch (Exception e) {
-            logger.error("Error while processing the request", e);
+            logger.error("Error while processing the request {}", e.getMessage());
             return new APIGatewayProxyResponseEvent()
                     .withStatusCode(400)
                     .withBody("Error processing the request");
@@ -64,7 +79,7 @@ public class PostItemHandler implements RequestHandler<APIGatewayProxyRequestEve
         try {
             dynamoDbClient.putItem(putItemRequest).get();
         } catch (InterruptedException | ExecutionException e) {
-            logger.error("Exception:", e.getMessage());
+            System.out.println("Exception:" + e.getMessage());
             throw new RuntimeException("Error creating Put Item request - " + e.getMessage());
         }
     }
