ac2d77402c0884a52f350fe183ae4bc3c25493df
Michael Costa <mdcs@amazon.com>
2023-10-21 15:12:44 -0300

    refactor starter solution

---
+++ code/powertools/netcore/SampleApp/src/LambdaHandlers/Common.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Microsoft.Extensions.DependencyInjection;
+using SampleApp.Repositories;
+
+public static class Common
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public static void Initialize(out ServiceProvider serviceProvider)
+    {
+        Console.WriteLine("Setting up the DI container");
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+
+        string region = Environment.GetEnvironmentVariable("AWS_REGION") ?? RegionEndpoint.USEast2.SystemName;
+        serviceCollection.AddSingleton<IAmazonDynamoDB>(new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region)))
+                        .AddScoped<IDynamoDBContext, DynamoDBContext>()
+                        .AddScoped<IItemRepository, ItemRepository>();
+        serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    public static APIGatewayProxyResponse APIResponse(HttpStatusCode httpStatusCode, string body)
+    {
+        var response = new APIGatewayProxyResponse
+        {
+            StatusCode = (int)httpStatusCode,
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+        if (!string.IsNullOrEmpty(body)) response.Body = body;
+
+        return response;
+    }
+
+    public static async Task<string> GetCallingIP()
+    {
+        try
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
+
+            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext: false);
+
+            return msg.Replace("\n", "");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
+}
---
+++ code/powertools/netcore/SampleApp/src/LambdaHandlers/GetAllItemsFunction.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
+using SampleApp.Repositories;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace LambdaHandlers
+{
+
+    public class GetAllItemsFunction
+    {
+        readonly ServiceProvider _serviceProvider;
+
+        public GetAllItemsFunction()
+        {
+            Common.Initialize(out _serviceProvider);
+        }
+
+        // GET api/items
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        {
+            var location = await Common.GetCallingIP();
+            Console.WriteLine("Getting ip address from external service");
+            Console.WriteLine($"Location: {location}");
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var itemRepository = scope.ServiceProvider.GetRequiredService<IItemRepository>();
+
+                var body = await itemRepository.GetItemsAsync();
+                var result = JsonSerializer.Serialize(body);
+
+                return Common.APIResponse(HttpStatusCode.OK, result);
+            }
+        }      
+
+    }
+}
